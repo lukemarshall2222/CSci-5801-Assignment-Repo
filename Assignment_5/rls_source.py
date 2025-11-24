71487c5d6cb6d362d9167edecabc5456b7b23140
lukemarshall2222 <lukemarshall2222@gmail.com>
2025-11-24 10:08:34 -0600

    add tests and reformat some of system code

---
+++ Assignment_5/rls_source.py
@@ -0,0 +1,52 @@
+import argparse
+
+class Artifact:
+    def __init__(self, red=True, green=True):
+        self.red: bool = red
+        self.green: bool = green
+
+def button_press(button_char, condition, pass_message, fail_message) -> bool:
+    user_input: str = " "
+    while user_input[0] != button_char:
+        user_input = input(f"Enter '{button_char}' to press the test button: ")
+    
+    if condition:
+        print(pass_message)
+        return True
+    else:
+        print(fail_message)
+        return False
+
+def system(launch_pad: Artifact, control_unit: Artifact):
+    test: bool = button_press('t', launch_pad.green, 'Circuit is functional', 'Error with circuit')
+    if not test:
+        return
+    
+    enable: bool = button_press('e', launch_pad.red, 'Communication is enabled', 'Error with communication')
+    if not enable:
+        return
+    
+    ready: bool = button_press('r', control_unit.red, 'We are ready to launch', 'We are not ready to launch')
+    if not ready:
+        return
+    
+    launch: bool = button_press('l', control_unit.green, 'Rocket is launching', 'Rocket is not ready to launch')
+    if not launch:
+        return
+
+def main():
+    launch_pad: Artifact = Artifact(args.communication_enabled, (args.battery_charged and args.circuit_closed))
+    control_unit: Artifact = Artifact(args.ready_to_launch, args.launch)
+    system(launch_pad, control_unit)
+      
+if __name__ == "__main__":
+    # Uer sets up arguments that they want to set up as false
+    parser = argparse.ArgumentParser(prog='rls_source')
+    parser.add_argument('--battery_charged', action='store_false', help="Is the battery charged")
+    parser.add_argument('--circuit_closed', action='store_false', help="Is the circuit closed?")
+    parser.add_argument('--communication_enabled', action='store_false', help="Can control unit and launch pad interact?")
+    parser.add_argument('--ready_to_launch', action='store_false', help="Does the launch pad know we are ready to launch?")
+    parser.add_argument('--launch', action='store_false', help="Can we actually launch the rocket?")
+    args = parser.parse_args()
+    main()
+
---
+++ Assignment_5/tests.py
@@ -0,0 +1,198 @@
+"""
+This module contains tests for the rls_source.py sourcecode file.
+"""
+
+import pytest
+from rls_source import Artifact, button_press, system
+
+class TestArtifact:
+    """tests for Artifact class"""
+
+    def test_battery_false(self):
+        """test launch pad light with battery_charged = False makes launch_pad.green == False"""
+        # battery_charged = False, circuit_closed = True
+        launch_pad = Artifact(True, (False and True))
+        assert launch_pad.red == True
+        assert launch_pad.green == False
+
+    def test_circuit_false(self):
+        """test launch pad light with  the circuit_closed = False makes launch_pad.green == False"""
+        # battery_charged = True, circuit_closed = False
+        launch_pad = Artifact(True, (True and False))
+        assert launch_pad.red == True
+        assert launch_pad.green == False
+
+    def test_circuit_true(self):
+        """test launch pad light with  both battery and cuituit True makes launch_pad.green == True"""
+        launch_pad = Artifact(True, (True and True))
+        assert launch_pad.red == True
+        assert launch_pad.green == True
+    
+    def test_ready_false(self):
+        """test control unit lights with ready_to_launch = False makes control_unit.red == False"""
+        # ready_to_launch = False, launch = True
+        control_unit = Artifact(False, True)
+        assert control_unit.red == False
+        assert control_unit.green == True
+    
+    def test_launch_false(self):
+        """test control unit lights with launch = False makes control_unit.green == False"""
+        # ready_to_launch = True, launch = False
+        control_unit = Artifact(True, False)
+        assert control_unit.red == True
+        assert control_unit.green == False
+    
+    def test_ready_launch_true(self):
+        """test control unit lights with both ready_to_launch = True and launch = True"""
+        # ready_to_launch = True, launch = True
+        control_unit = Artifact(True, True)
+        assert control_unit.red == True
+        assert control_unit.green == True
+
+
+class TestButtonPress:
+    """tests for button_press() function"""
+
+    def test_button_simple_true(self, monkeypatch, capsys):
+        """correct input on first try, result is true"""
+        monkeypatch.setattr('builtins.input', lambda _: 't')
+
+        result = button_press('t', True, 'Success', 'Failure')
+
+        assert result == True
+        captured = capsys.readouterr()
+        assert 'Success' in captured.out
+
+    def test_button_simple_false(self, monkeypatch, capsys):
+        """correct input on first try, result is false"""
+        monkeypatch.setattr('builtins.input', lambda _: 't')
+
+        result = button_press('t', False, 'Success', 'Failure')
+
+        assert result == False
+        captured = capsys.readouterr()
+        assert 'Failure' in captured.out
+
+    def test_button_complex_true(self, monkeypatch, capsys):
+        """test if button behaves correctly in face of multiple inccorect input followed by correct resulting in true"""
+        inputs = iter(['x', 'y', 't'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+        result = button_press('t', True, 'Success', 'Failure')
+
+        assert result == True
+        captured = capsys.readouterr()
+        assert 'Success' in captured.out
+
+    def test_button_complex_false(self, monkeypatch, capsys):
+        """Test if button behaves correctly in face of multiple incorrect input followed by correct resulting in false"""
+        inputs = iter(['a', 'b', 't'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+        result = button_press('t', False, 'Success', 'Failure')
+
+        assert result == False
+        captured = capsys.readouterr()
+        assert 'Failure' in captured.out
+
+
+class TestSystem:
+    """test system() function"""
+
+    def test_system_all_success(self, monkeypatch, capsys):
+        """total launch sequence, all steps should complete normally"""
+        inputs = iter(['t', 'e', 'r', 'l'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+        launch_pad = Artifact(red=True, green=True)
+        control_unit = Artifact(red=True, green=True)
+
+        system(launch_pad, control_unit)
+
+        captured = capsys.readouterr()
+        assert 'Circuit is functional' in captured.out
+        assert 'Communication is enabled' in captured.out
+        assert 'We are ready to launch' in captured.out
+        assert 'Rocket is launching' in captured.out
+
+    def test_system_test_fail(self, monkeypatch, capsys):
+        """should fail at test button"""
+        monkeypatch.setattr('builtins.input', lambda _: 't')
+
+        # signifies batteries are not charged or the circuit is not closed, causes test to fail
+        launch_pad = Artifact(red=True, green=False)
+        control_unit = Artifact(red=True, green=True)
+
+        system(launch_pad, control_unit)
+
+        captured = capsys.readouterr()
+        assert 'Error with circuit' in captured.out
+        # success response not reached
+        assert 'Communication is enabled' not in captured.out
+
+    def test_system_enable_fail(self, monkeypatch, capsys):
+        """should fail at enable button"""
+        inputs = iter(['t', 'e'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+        # signifies the communication enabling should fail because it is not enabled at a higher level
+        launch_pad = Artifact(red=False, green=True) 
+        control_unit = Artifact(red=True, green=True)
+
+        system(launch_pad, control_unit)
+
+        captured = capsys.readouterr()
+        assert 'Circuit is functional' in captured.out
+        assert 'Error with communication' in captured.out
+        assert 'We are ready to launch' not in captured.out  # success response not reached
+
+    def test_system_ready_fail(self, monkeypatch, capsys):
+        """should fail at ready button"""
+        inputs = iter(['t', 'e', 'r'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+        launch_pad = Artifact(red=True, green=True)
+        # signifies the ready communication should fail because it is not enabled at a higher level
+        control_unit = Artifact(red=False, green=True)
+
+        system(launch_pad, control_unit)
+
+        captured = capsys.readouterr()
+        assert 'Circuit is functional' in captured.out
+        assert 'Communication is enabled' in captured.out
+        assert 'We are not ready to launch' in captured.out
+        assert 'Rocket is launching' not in captured.out  # success response not reached
+
+    def test_system_fail_launch_fail(self, monkeypatch, capsys):
+        """should fail at launch"""
+        inputs = iter(['t', 'e', 'r', 'l'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+        launch_pad = Artifact(red=True, green=True)
+        # signifies the launch communication should fail because it is not enabled at a higher level
+        control_unit = Artifact(red=True, green=False)
+
+        system(launch_pad, control_unit)
+
+        captured = capsys.readouterr()
+        assert 'Circuit is functional' in captured.out
+        assert 'Communication is enabled' in captured.out
+        assert 'We are ready to launch' in captured.out
+        assert 'Rocket is not ready to launch' in captured.out
+
+    def test_system_random_input_success(self, monkeypatch, capsys):
+        """random input (noise) interleaved into the launch sequence should not imact the launch if all signals given in correct order
+        -- possible place for improvement, could keep track of noise and if it exceeds some bound the launch is scrapped"""
+        inputs = iter(['x', 't', 'y', 'e', 'z', 'r', 'a', 'l'])
+        monkeypatch.setattr('builtins.input', lambda _: next(inputs))
+
+        launch_pad = Artifact(red=True, green=True)
+        control_unit = Artifact(red=True, green=True)
+
+        system(launch_pad, control_unit)
+
+        captured = capsys.readouterr()
+        assert 'Rocket is launching' in captured.out
+
+
+
